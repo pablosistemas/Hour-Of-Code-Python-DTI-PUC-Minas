2c6e3ca5b535c8dcd0d1abba70c751437a0cdda1
Pablo <bob@site1.fogbow>
2019-10-12 10:50:20 -0300

    Gateway com threads

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ class Gateway:
         self.host = HOST
         self.port = PORT
         self.tcp_sock = None
+        self.threads = []
         self.__create_socket__()
     
     def __create_socket__(self):
@@ -15,19 +16,29 @@ class Gateway:
         src = (self.host, self.port)
         self.tcp_sock.bind(src)
 
+    def print_client_messages(self, con, client):
+        msg = con.recv(1024)
+        while msg:
+            print (client, msg)
+            msg = con.recv(1024)
+
+        print ('Finalizando conexao do cliente', client)
+        con.close()
+
+    def shutdown_server(self):
+        for thread in self.threads:
+            thread.join()
+        return
+
     def up_server(self):
         self.tcp_sock.listen()
         print("I am waiting for new clients")
         while True:
             con, client = self.tcp_sock.accept()
             print ('Conectado por', client)
-            msg = con.recv(1024)
-            while msg:
-                print (client, msg)
-                msg = con.recv(1024)
-
-            print ('Finalizando conexao do cliente', client)
-            con.close()
+            t = threading.Thread(target=self.print_client_messages, args=(con, client,))
+            t.start()
+            self.threads.append(t)
 
 
 def main():
